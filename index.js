e1b3be4629ea71475f4be6a7fc625314c9c75fa6
ezequielgame <ezequiel.gmejia@gmail.com>
2024-06-30 13:32:30 -0600

    chore: add eslint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import { execFileSync } from 'child_process';
-import { format } from 'util';
+import { execFileSync } from 'child_process'
+import { format } from 'util'
 
 export class BranchNameLint {
-  constructor(options) {
+  constructor (options) {
     const defaultOptions = {
       prefixes: ['feature', 'hotfix', 'release', 'bugfix', 'issue'],
       suggestions: {
-        features: 'feature', feat: 'feature', fix: 'hotfix', releases: 'release',
+        features: 'feature',
+        feat: 'feature',
+        fix: 'hotfix',
+        releases: 'release',
       },
       banned: ['wip'],
       skip: ['skip-ci'],
@@ -18,50 +21,50 @@ export class BranchNameLint {
       msgPrefixSuggestion: 'Instead of "%s" try "%s".',
       msgDoesNotMatchRegex: 'Branch "%s" does not match the allowed pattern: "%s"',
       msgSeparatorRequired: 'Branch "%s" must contain a separator "%s".',
-    };
-
-    this.options = Object.assign(defaultOptions, options);
-    this.branch = this.getCurrentBranch();
-    this.ERROR_CODE = 1;
-    this.SUCCESS_CODE = 0;
-  }
-
-  validateWithRegex() {
-    if (this.options.regex) {
-      const REGEX = new RegExp(this.options.regex, this.options.regexOptions);
-      return REGEX.test(this.branch);
     }
 
-    return true;
+    this.options = Object.assign(defaultOptions, options)
+    this.branch = this.getCurrentBranch()
+    this.ERROR_CODE = 1
+    this.SUCCESS_CODE = 0
   }
 
-  doValidation() {
-    const parts = this.branch.split(this.options.separator);
-    const prefix = parts[0];
-    let name = null;
+  validateWithRegex () {
+    if (this.options.regex) {
+      const REGEX = new RegExp(this.options.regex, this.options.regexOptions)
+      return REGEX.test(this.branch)
+    }
+
+    return true
+  }
+
+  doValidation () {
+    const parts = this.branch.split(this.options.separator)
+    const prefix = parts[0]
+    let name = null
     if (parts[1]) {
-      const [, second] = parts;
-      name = second;
+      const [, second] = parts
+      name = second
     }
 
     if (this.options.skip.length > 0 && this.options.skip.includes(this.branch)) {
-      return this.SUCCESS_CODE;
+      return this.SUCCESS_CODE
     }
 
     if (this.options.banned.includes(this.branch)) {
-      return this.error(this.options.msgBranchBanned, this.branch);
+      return this.error(this.options.msgBranchBanned, this.branch)
     }
 
     if (this.options.disallowed.includes(this.branch)) {
-      return this.error(this.options.msgBranchDisallowed, this.branch);
+      return this.error(this.options.msgBranchDisallowed, this.branch)
     }
 
     if (this.branch.includes(this.options.separator) === false) {
-      return this.error(this.options.msgSeparatorRequired, this.branch, this.options.separator);
+      return this.error(this.options.msgSeparatorRequired, this.branch, this.options.separator)
     }
 
     if (!this.validateWithRegex()) {
-      return this.error(this.options.msgBranchDisallowed, this.branch, this.options.regex);
+      return this.error(this.options.msgBranchDisallowed, this.branch, this.options.regex)
     }
 
     if (this.options.prefixes.includes(prefix) === false) {
@@ -69,28 +72,28 @@ export class BranchNameLint {
         this.error(
           this.options.msgPrefixSuggestion,
           [prefix, name].join(this.options.separator),
-          [this.options.suggestions[prefix], name].join(this.options.separator),
-        );
+          [this.options.suggestions[prefix], name].join(this.options.separator)
+        )
       } else {
-        this.error(this.options.msgPrefixNotAllowed, prefix);
+        this.error(this.options.msgPrefixNotAllowed, prefix)
       }
 
-      return this.ERROR_CODE;
+      return this.ERROR_CODE
     }
 
-    return this.SUCCESS_CODE;
+    return this.SUCCESS_CODE
   }
 
-  getCurrentBranch() {
-    const branch = execFileSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']).toString();
-    this.branch = branch;
-    return this.branch.trim();
+  getCurrentBranch () {
+    const branch = execFileSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']).toString()
+    this.branch = branch
+    return this.branch.trim()
   }
 
-  error() {
-    console.error('Branch name lint fail!', Reflect.apply(format, null, arguments)); // eslint-disable-line prefer-rest-params
-    return this.ERROR_CODE;
+  error () {
+    console.error('Branch name lint fail!', Reflect.apply(format, null, arguments))
+    return this.ERROR_CODE
   }
 }
 
-export default BranchNameLint;
+export default BranchNameLint
