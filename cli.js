8818f7b4136d846c27b5c8f1af3a120ad4240bbf
ezequielgame <ezequiel.gmejia@gmail.com>
2024-06-30 12:18:31 -0600

    chore: remove tests

--- cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import { fork } from 'child_process';
-
-process.emitWarning('branch-name-lint will stop using ./cli.js path in the future. Please use npx branch-name-lint');
-
-fork('./bin/branch-name-lint');
